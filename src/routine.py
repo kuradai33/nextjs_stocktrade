af0988f4f28adf922ad43c924703010b78e091c0
kurad <106243129+kuradai33@users.noreply.github.com>
2024-10-05 04:59:47 +0900

    定期的にDBの更新を行う処理を作成

---
+++ src/routine.py
@@ -0,0 +1,32 @@
+import asyncio
+import time
+import datetime as dt
+import schedule
+from prisma import Prisma
+
+from libpy import mylib
+
+async def updateDetaBase():
+    prisma = Prisma()
+    await prisma.connect()
+
+    todayDatetime = dt.datetime.now()
+    today = dt.date(todayDatetime.year, todayDatetime.month, todayDatetime.day)
+
+    symbolDatas = await prisma.stocks.find_many()
+    cnt = 0
+    cntAllSymbol = len(symbolDatas)
+    for data in symbolDatas:
+        code = data.code
+        await mylib.updateDB(code, "2024-01-01", (today + dt.timedelta(days=1)).strftime(mylib.getDateformat()))
+        cnt += 1
+        print(cnt, "/", cntAllSymbol)
+        print(code, "を取得しました")
+        time.sleep(3) # 待機
+        
+asyncio.run(updateDetaBase())
+
+if False: # Debug
+    schedule.every().day.at("18:27").do(updateDetaBase)
+else:
+    schedule.every().day.at("18:00").do(updateDetaBase)
