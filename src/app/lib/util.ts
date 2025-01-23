8976baad5377217649b21a3d4ee8c61fad0d9c6c
kurad <106243129+kuradai33@users.noreply.github.com>
2025-01-24 00:11:36 +0900

    テスト結果の同値判定を正確にした・DateStrと文字列の変換メソッドを作成

--- src/app/lib/util.ts
+++ src/app/lib/util.ts
@@ -20,10 +20,30 @@ export class TestResult{
         this.expectedResult_ = expectedResult;
 
         // 出力とエラーが正しいか確認する
-        if(result.output == expectedResult.output && result.error == expectedResult.error){
+        if(this.checkEqualData(result.output, expectedResult.output) &&
+           this.checkEqualData(result.error,  expectedResult.error )){
             this.isCorrect_ = true;
         }
         else this.isCorrect_ = false;
+    }
+
+    private checkEqualData(data1: any, data2: any){
+        if(data1 === null && data2 === null) return true;
+
+        const isArrayBothData = Array.isArray(data1) && Array.isArray(data2);
+        // objectと判定されるのは配列とnullとオブジェクト
+        const isObjectBothData = typeof data1 == "object" && typeof data2 == "object";
+        if(isArrayBothData || isObjectBothData){
+            // Object型はJson形式の文字列に変換して比較
+            return JSON.stringify(data1) == JSON.stringify(data2);
+        }
+
+        // Enhance: 関数の同値判定を追加
+        if(typeof data1 == "function" && typeof data2 == "function"){
+            throw "TypeError: 関数同士を比較することができません";
+        }
+
+        return data1 === data2
     }
 
     // 各メンバ変数の値を返す
@@ -94,4 +114,54 @@ export function checkComposedUsableChar(checkedStr: string, usableChars: string)
         }
     }
     return true;
+}
+
+// データがDateStrであるまたは配列やオブジェクトの要素にDataStrが含まれていたらstringに変換する
+// Json形式にするときに使用する
+export function convertDateStrToString(data: any): any {
+    // DateStrはtypeofでobjectと判定されるからその前に置く
+    // 元に戻せるように文字列に印を付ける
+    if(data instanceof DateStr){
+        return `${data.getDateStr()}-**DateStr**`;
+    }
+    else if(data == null) return data;
+    else if(Array.isArray(data)){
+        return data.map((v, _) => convertDateStrToString(v));
+    }
+    // objectと判定されるのは配列とnullとオブジェクト
+    else if(typeof data == "object"){
+        let newObject: {[K: string]: any} = {};
+        for(const key in data){
+            const value = data[key];
+            newObject[key] = convertDateStrToString(value);
+        }
+        return newObject;
+    }
+    else return data;
+}
+
+// データの元がDateStrであるまたは配列やオブジェクトの要素にそれが含まれていたらDateStrに変換する
+// Json形式から戻すときに使用する
+export function convertSpecificStringToDateStr(data: any): any { 
+    if(typeof data == "string"){
+        const hyphenSplitStrData = data.split("-");
+        if(hyphenSplitStrData[hyphenSplitStrData.length - 1] == "**DateStr**"){
+            return new DateStr(data.slice(0, data.length - 12));
+        }
+        else return data;
+    }
+    else if(data == null) return data;
+    else if(Array.isArray(data)){
+        return data.map((v, _) => convertSpecificStringToDateStr(v));
+    }
+    // objectと判定されるのは配列とnullとオブジェクト
+    else if(typeof data == "object"){
+        let newObject: {[K: string]: any} = {};
+        for(const key in data){
+            const value = data[key];
+            newObject[key] = convertSpecificStringToDateStr(value);
+        }
+        return newObject;
+    }
+    else return data;
 }
