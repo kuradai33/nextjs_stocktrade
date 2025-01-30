b7bba75219806b518a5660daff44d12f50b4ff30
kurad <106243129+kuradai33@users.noreply.github.com>
2025-01-31 03:23:16 +0900

    コメント・例外処理を追加

--- src/registerSymbol.py
+++ src/registerSymbol.py
@@ -4,22 +4,27 @@ import csv
 import asyncio
 from prisma import Prisma
 
-# url = "https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls"
-# r = requests.get(url)
-# with open('data_j.xls', 'wb') as output:
-#     output.write(r.content)
-# stocklist = pd.read_excel("./data_j.xls")
-# stocklist.loc[stocklist["市場・商品区分"]=="市場第一部（内国株）",
-#               ["コード","銘柄名","33業種コード","33業種区分","規模コード","規模区分"]
-#              ]
+# 最新データを取得してデータベースに銘柄名と銘柄コードを新たに追加する
 
-# # ETF等を除く
-# codes = stocklist[(stocklist['市場・商品区分'] == 'プライム（内国株式）') | (stocklist['市場・商品区分'] == 'スタンダード（内国株式）') | (stocklist['市場・商品区分'] == 'グロース（内国株式）')]
-# len(codes)
+# excelデータとしてデータを取得
+url = "https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls"
+r = requests.get(url)
+with open('data_j.xls', 'wb') as output:
+    output.write(r.content)
+stocklist = pd.read_excel("./data_j.xls")
+stocklist.loc[stocklist["市場・商品区分"]=="市場第一部（内国株）",
+              ["コード","銘柄名","33業種コード","33業種区分","規模コード","規模区分"]
+             ]
 
-filename = 'jpx_code2024_8.csv'
-# codes.to_csv('./src/datas/rawCSV/' + filename)
+# ETF等を除く
+codes = stocklist[(stocklist['市場・商品区分'] == 'プライム（内国株式）') | (stocklist['市場・商品区分'] == 'スタンダード（内国株式）') | (stocklist['市場・商品区分'] == 'グロース（内国株式）')]
+len(codes)
 
+# excelファイルをcsv化して保存
+filename = 'jpx_code2025_1.csv'
+codes.to_csv('./src/datas/rawCSV/' + filename)
+
+# データベースに登録
 async def main() -> None:
     prisma = Prisma()
     await prisma.connect()
@@ -31,13 +36,16 @@ async def main() -> None:
             if header:
                 header = False
                 continue
-
-            user = await prisma.stocks.create(
-                data={
-                    "code": line[2],
-                    "name": line[3]
-                },
-            )
+            try:
+                user = await prisma.stocks.create(
+                    data={
+                        "code": line[2],
+                        "name": line[3]
+                    }
+                )
+            except:
+                pass
+            
 
     await prisma.disconnect()
 
