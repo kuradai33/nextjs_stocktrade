90c2943b6c9657a9d823e7ca71b68214f927fc51
kurad <106243129+kuradai33@users.noreply.github.com>
2025-02-14 01:38:28 +0900

    データベース更新用コードを新たに作成

--- src/routine.py
+++ src/routine.py
@@ -31,3 +31,26 @@ asyncio.run(updateDetaBase())
 #     schedule.every().day.at("18:27").do(updateDetaBase)
 # else:
 #     schedule.every().day.at("18:00").do(updateDetaBase)
+
+async def updateLatestDB():
+    # prismaに接続
+    await prisma.connect()
+
+    # 今日の日付を取得
+    todayDatetime = dt.datetime.now()
+    today = dt.date(todayDatetime.year, todayDatetime.month, todayDatetime.day)
+    todayStr = (today + dt.timedelta(days=1)).strftime(mylib.getDateformat())
+
+    # 株式データを取得
+    symbolDatas = await prisma.stocks.find_many()
+    cnt = 0
+    cntAllSymbol = len(symbolDatas)
+    for data in symbolDatas:
+        cnt += 1
+        print(cnt, "/", cntAllSymbol)
+        code = data.code
+        id = data.id
+        await mylib.updateStockPricesDB(code, id, "2019-01-01", todayStr)
+        await prisma.disconnect()
+        break
+        time.sleep(5) # 待機
